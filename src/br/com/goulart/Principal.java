3caca8d1649136932ca798a2ae8b43ff83421cc6
LGoulart <leandrovgoulart@gmail.com>
2024-04-25 23:14:18 -0300

    expandido o numero de moedas

--- src/br/com/goulart/Principal.java
+++ src/br/com/goulart/Principal.java
@@ -16,47 +16,66 @@ public class Principal {
                     *=============================================================*
                     * Bem-vindo ao Conversor de Moedas - Desenvolvido por Goulart *
                     *-------------------------------------------------------------*
-                    *  (1) DÓLAR AMERICANO =>  PESO ARGENTINO                     *
-                    *  (2) PESO ARGENTINO  =>  DÓLAR AMERICANO                    *
-                    *  (3) DÓLAR AMERICANO =>  REAL BRASILEIRO                    *
-                    *  (4) REAL BRASILEIRO =>  DÓLAR AMERICANO                    *
-                    *  (5) DÓLAR AMERICANO =>  PESO COLOMBIANO                    *
-                    *  (6) PESO COLOMBIANO =>  DÓLAR AMERICANO                    *
-                    *  (7) SAIR DO PROGRAMA                                       *
+                    *   (1) BRL Real Brasileiro   =>    Dólar Americano USD       *
+                    *   (2) USD Dólar Americano   =>    Real Brasileiro BRL       *
+                    *   (3) BRL Real Brasileiro   =>    Euro EUR                  *
+                    *   (4) EUR Euro              =>    Real Brasileiro BRL       *
+                    *   (5) BRL Real Brasileiro   =>    Libra Esterlina GBP       *
+                    *   (6) GBP Libra Esterlina   =>    Real Brasileiro BRL       *
+                    *   (7) BRL Real Brasileiro   =>    Peso Argentino ARS        *
+                    *   (8) ARS Peso Argentino    =>    Real Brasileiro BRL       *
+                    *   (9) BRL Real Brasileiro   =>    Dólar Canadense CAD       *
+                    *  (10) CAD Dólar Canadense   =>    Real Brasileiro BRL       *
+                    *  (11) SAIR DO PROGRAMA                                      *
                     *=============================================================*
                     Por Favor, Digite a Opção Desejada: """);
 
-
             opcao = entrada.nextInt();
 
-            if(opcao == 7) {
+            if(opcao == 11) {
                 break;
             }
 
             switch (opcao) {
                 case 1:
-                    conversor.moedaBase = "USD";
-                    conversor.moedaAlvo = "ARS";
-                    break;
-                case 2:
-                    conversor.moedaBase = "ARS";
-                    conversor.moedaAlvo = "USD";
-                    break;
-                case 3:
-                    conversor.moedaBase = "USD";
-                    conversor.moedaAlvo = "BRL";
-                    break;
-                case 4:
                     conversor.moedaBase = "BRL";
                     conversor.moedaAlvo = "USD";
                     break;
-                case 5:
+                case 2:
                     conversor.moedaBase = "USD";
-                    conversor.moedaAlvo = "COP";
+                    conversor.moedaAlvo = "BRL";
+                    break;
+                case 3:
+                    conversor.moedaBase = "BRL";
+                    conversor.moedaAlvo = "EUR";
+                    break;
+                case 4:
+                    conversor.moedaBase = "EUR";
+                    conversor.moedaAlvo = "BRL";
+                    break;
+                case 5:
+                    conversor.moedaBase = "BRL";
+                    conversor.moedaAlvo = "GBP";
                     break;
                 case 6:
-                    conversor.moedaBase = "COP";
-                    conversor.moedaAlvo = "USD";
+                    conversor.moedaBase = "GBP";
+                    conversor.moedaAlvo = "BRL";
+                    break;
+                case 7:
+                    conversor.moedaBase = "BRL";
+                    conversor.moedaAlvo = "ARS";
+                    break;
+                case 8:
+                    conversor.moedaBase = "ARS";
+                    conversor.moedaAlvo = "BRL";
+                    break;
+                case 9:
+                    conversor.moedaBase = "BRL";
+                    conversor.moedaAlvo = "CAD";
+                    break;
+                case 10:
+                    conversor.moedaBase = "CAD";
+                    conversor.moedaAlvo = "BRL";
                     break;
                 default:
                     System.out.println("Opção inválida! Por favor, verifique.");
@@ -64,11 +83,11 @@ public class Principal {
             }
 
             System.out.println("Digite o valor que deseja converter: ");
-            double valor = entrada.nextDouble();
+            double quantidade = entrada.nextDouble();
 
-            RespostaParesMoedas resultado = conversor.obterParMoedas(valor);
+            RespostaParesMoedas resultado = conversor.obterParMoedas(quantidade);
 
-            System.out.println("O valor de " + conversor.moedaBase + " " + valor +
+            System.out.println("O valor de " + conversor.moedaBase + " " + quantidade +
                     " para " + conversor.moedaAlvo + " é " + resultado.conversion_result);
         }
     }
